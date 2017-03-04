892cf78da1b0ebcbe781c9329cdc7578490a9bd3
Jeanette Rojas <jrojas@aclanyc.com>
2017-03-04 03:09:07 +0000

    its a house

---
+++ script.js
@@ -0,0 +1,19 @@
+ TinyTurtle.apply(window); 
+ 
+ forward(50);
+ right(90);
+ forward(50);
+ right(90);
+ forward(50);
+ right(90); 
+ forward(50);
+ right(90);
+ forward(50);
+ 
+ 
+ right(50);
+ forward(35);
+ right(80);
+ forward(35);
+ 
+ 
